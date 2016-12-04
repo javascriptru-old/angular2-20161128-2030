18964e5495c7f74c1c35f2cd63b896a8142af75e
al-tim <alexey.git@mail.ru>
2016-12-04 04:14:41 +0300

    1) A bit of code clean up 2) Moved utilities into a separate file

--- homework/lesson1-ts/al.tim/homework.ts
+++ homework/lesson1-ts/al.tim/homework.ts
@@ -1,69 +1,68 @@
+import {HomeworkUtils} from './HomeworkUtils';
+
 interface IEmployee {
-    employeeId: number;
+    id: number;
     firstName: string;
     lastName: string;
-    averageMonthlyPayHome: number;
+    fullNameUpperCase: string;
+    averageMonthlyPay: number;
 }
 
 class Employee implements IEmployee {
-    public employeeId: number;
-    public firstName: string;
-    public lastName: string;
-    public averageMonthlyPayHome: number;
+    public id: number;
+    private _firstName: string;
+    private _lastName: string;
+    public averageMonthlyPay: number;
+    private _fullNameUpperCase: string;
+
+    get fullNameUpperCase() {//lazy initialization for comparator, other getters and setters are for resetting lazy initialization
+        if (!this._fullNameUpperCase) {
+            this._fullNameUpperCase = (this._firstName+" "+this._lastName).toUpperCase();
+        }
+        return this._fullNameUpperCase;
+    }
+    get firstName() {
+        return this._firstName;
+    }
+    set firstName(newValue: string) {
+        this._firstName = newValue;
+        this._fullNameUpperCase = undefined;
+    }
+    get lastName() {
+        return this._lastName;
+    }
+    set lastName(newValue: string) {
+        this._lastName = newValue;
+        this._fullNameUpperCase = undefined;
+    }
 }
 
-class PermanentEmployee extends Employee implements IEmployee {
-}
+class PermanentEmployee extends Employee {}
 
-class TermContractEmployee extends Employee implements IEmployee {
+class TermContractEmployee extends Employee {
     set hourlyPayRate(hourlyPayRate: number) {
-        this.averageMonthlyPayHome = 20.8*8*hourlyPayRate;
-    }
-}
-
-class HomeworkUtils {
-    static toInstance<T>(targetObject: T, sourceObject: any) : T {
-        for (var propName in sourceObject) {
-            targetObject[propName] = sourceObject[propName]
-        }
-
-        return targetObject;
-    }
-    static printObject(object: any, propertyNames: string[], prefix) {
-        let toPrint:string;
-        for (const propertyName of propertyNames) {
-            toPrint = (toPrint ? toPrint+ "; " : "")+ propertyName+":"+object[propertyName];
-        }
-        console.log((prefix?prefix:"")+toPrint);
+        this.averageMonthlyPay = 20.8*8*hourlyPayRate;
     }
 }
 
 declare function require(name:string);
 let employees: IEmployee[] = [];
 for (const employeeItem of JSON.parse(require("fs").readFileSync("./employees.json", 'utf8')).employees) {
-    let newIEmployeeItem: IEmployee = HomeworkUtils.toInstance((employeeItem.hourlyPayRate)?new TermContractEmployee():new PermanentEmployee(), employeeItem);
-    employees.push(newIEmployeeItem);
+    employees.push(HomeworkUtils.copyProperties((employeeItem.hourlyPayRate)?new TermContractEmployee():new PermanentEmployee(), employeeItem));
 }
 
 let employeesSortedByPay = employees.sort((employee1:IEmployee, employee2:IEmployee) => {
-    let compareValue = employee2.averageMonthlyPayHome-employee1.averageMonthlyPayHome;
-    if (compareValue === 0) {
-        compareValue = (employee1.firstName+" "+employee1.lastName).toUpperCase().localeCompare((employee2.firstName+" "+employee2.lastName).toUpperCase());
-    }
-    return compareValue;
+    let compareValue = employee2.averageMonthlyPay-employee1.averageMonthlyPay;
+    return (compareValue === 0) ? compareValue = employee1.fullNameUpperCase.localeCompare(employee2.fullNameUpperCase) : compareValue;
 });
 
 console.log("All employees sorted by pay, then name:");
-for (const employee of employeesSortedByPay) {
-    HomeworkUtils.printObject(employee, ['employeeId', 'firstName', 'lastName', 'averageMonthlyPayHome'], "  ");
-}
+HomeworkUtils.printObjectArray(employeesSortedByPay, ['id', 'firstName', 'lastName', 'averageMonthlyPay'], [5, 20, 20, 20], 2);
 
 console.log("First five employees sorted by pay, then name:");
-for (const employee of employeesSortedByPay.slice(0, 5)) {
-    HomeworkUtils.printObject(employee, ['employeeId', 'firstName', 'lastName', 'averageMonthlyPayHome'], "  ");
-}
+HomeworkUtils.printObjectArray(employeesSortedByPay.slice(0, 5), ['id', 'firstName', 'lastName', 'averageMonthlyPay'], [5, 20, 20, 20], 2);
 
 console.log("Ids of the last three employees sorted by pay, then name:");
 for (const employee of employeesSortedByPay.slice(employeesSortedByPay.length-3, employeesSortedByPay.length)) {
-    HomeworkUtils.printObject(employee, ['employeeId'], "  ");
+    HomeworkUtils.printObject(employee, ['id'], 2);
 }
