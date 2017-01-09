8e56ebf0932ac27629f808bcf311addef5b7f491
Stepan Suvorov <stevermeister@gmail.com>
2017-01-09 20:00:32 +0200

    subject example

--- homework/lesson6-services/pavel.leonov/typeahead/src/app/typeahead/typeahead.component.ts
+++ homework/lesson6-services/pavel.leonov/typeahead/src/app/typeahead/typeahead.component.ts
@@ -1,5 +1,6 @@
 import { Observable } from 'rxjs/Observable';
 import { Observer } from 'rxjs/Observer';
+import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/filter';
@@ -14,18 +15,62 @@ import { ITypeaheadResult } from './result.interface';
   styleUrls: ['./typeahead.component.css']
 })
 export class TypeaheadComponent implements OnInit {
-  inputStream: Observable<string>;
+  inputStream: Subject<string>;
   inputObserver: Observer<string>;
 
   dataList: ITypeaheadResult[];
+
+  o: Observable<string>;
+  oo: Observer<string>;
+  s: Subject<string>;
 
   @Input('searchFn') searchFn: ISearchFn;
   @Input('placeholder') placeholder: string;
 
   constructor() {
-    this.inputStream = new Observable((observer: Observer<string>) => {
-      this.inputObserver = observer;
+    // this.inputStream = new Observable((observer: Observer<string>) => {
+    //   this.inputObserver = observer;
+    // });
+
+    // this.o = Observable.create((observer: Observer<string>) => {
+    //   observer.next('Hello');
+    //   setTimeout(() => { observer.next('Hello'); },2000);
+    //   setTimeout(() => { observer.next('Hello'); },2000);
+    //   setTimeout(() => { observer.next('Hello'); },2000);
+    //   setTimeout(() => { observer.next('Hello'); },2000);
+    //   observer.complete();
+    //   this.oo = observer;
+    // });
+
+    this.s = new Subject();
+    this.s.next('Hello');
+    setTimeout(() => { this.s.next('Hello'); },2000);
+
+    //UserService//getUserObservable//http
+
+    //component1
+    this.s.subscribe((message) => {
+      console.log(message);
     });
+    
+    //component2
+    this.s.subscribe((message) => {
+      console.log(message);
+    });
+
+    //component3
+    this.s.subscribe((message) => {
+      console.log(message);
+    });
+
+    //component1
+    this.s.next('Hi!');
+
+    //component5
+    this.s.next('How do you do!');
+
+
+    this.inputStream = new Subject();
 
     this.dataList = [];
 
@@ -38,7 +83,8 @@ export class TypeaheadComponent implements OnInit {
   }
 
   onInput(query: string) {
-    this.inputObserver.next(query);
+    //this.inputObserver.next(query);
+    this.inputStream.next(query);
   }
 
   ngOnInit() {
